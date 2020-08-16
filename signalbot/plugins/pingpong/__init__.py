c28bcf8b5bd00d5c014064fae587d1710df291f7
Sam Johnson <sam@johnson.io>
2020-08-15 22:04:41 -0700

    Tweak pingpong to preface with /bot and add sender on reply

--- signalbot/plugins/pingpong/__init__.py
+++ signalbot/plugins/pingpong/__init__.py
@@ -4,10 +4,10 @@ from signalbot.plugins import PluginChat
 class PingPongChat(PluginChat):
 
     def triagemessage(self, message):
-        if message.text != 'ping':
+        if message.text != '/bot ping':
             return
 
-        self.reply('pong')
+        self.reply(f'{message.sender}: pong')
 
 
 __plugin_chat__ = PingPongChat
